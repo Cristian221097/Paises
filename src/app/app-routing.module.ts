4a51f622b65b34ce79f7eef9b40bb77eb2601a51
Cristian Amancio <cristianjoelamancioherrera@gmail.com>
2021-02-26 01:25:43 +0800

    maquetando cabecera

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {InicioComponent} from './Componentes/inicio/inicio.component';
+import {PaisesComponent} from './Componentes/paises/paises.component';
+
+const routes: Routes = [
+  {path:'',component:InicioComponent},
+  {path:'Paises',component:PaisesComponent},
+  {path:'Inicio',component:InicioComponent},
+  {path:'**',component:InicioComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaisesComponent } from './Componentes/paises/paises.component';
+import { InicioComponent } from './Componentes/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaisesComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
