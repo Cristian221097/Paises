d356a52e53e153b19fdf574fbe64bbc29a22ef9d
Cristian Amancio <cristianjoelamancioherrera@gmail.com>
2021-03-03 11:36:59 +0800

    maquetando frontera idioma etc

--- src/app/Componentes/leguanjes/leguanjes.component.ts
+++ src/app/Componentes/leguanjes/leguanjes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-leguanjes',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leguanjes.component.css']
 })
 export class LeguanjesComponent implements OnInit {
+
+  @Input() informacion:any;
 
   constructor() { }
 
